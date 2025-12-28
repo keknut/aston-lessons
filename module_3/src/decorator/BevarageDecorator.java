576c85e6e2c319c0483973ff68e43387f1bb7496
n.mishenko <n.mishenko@kt.team>
2025-12-29 03:42:09 +0400

    [keknut] добавил декораторв в module_3

--- module_3/src/decorator/BevarageDecorator.java
+++ module_3/src/decorator/BevarageDecorator.java
@@ -6,5 +6,4 @@ public abstract class BevarageDecorator implements Bevarage{
     public BevarageDecorator(Bevarage bevarage) {
         this.bevarage = bevarage;
     }
-
 }
--- module_3/src/strategy/Context.java
+++ module_3/src/strategy/Context.java
@@ -14,5 +14,4 @@ public class Context {
             throw new IllegalStateException("Strategy not set");
         }
     }
-
 }
